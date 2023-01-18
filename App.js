5b7281157ea500b4944055708c29841ae73a0f81
nazmulrony <nhrony65@gmail.com>
2023-01-18 14:53:43 +0600

    custom radio button created

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import RadioButtons from "./components/RadioButtons";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const items = [
+        { value: "seller", label: "seller" },
+        { value: "buyer", label: "buyer" },
+        { value: "admin", label: "admin" },
+        { value: "admin2", label: "admin2" },
+    ];
+    const [value, setValue] = useState(null);
+    console.log(value);
+    return (
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+            <RadioButtons
+                items={items}
+                color="#4CAF50"
+                // direction="row"
+                // gap={22}
+                onPress={(value) => setValue(value)}
+                initial={value}
+            />
+            <Text>Dummy text</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ components/RadioButtons.js
@@ -0,0 +1,74 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+
+const RadioButtons = ({ items, onPress, initial, color, direction, gap }) => {
+    const lastChildIndex = items.length - 1;
+
+    return (
+        <View style={[styles.container, { flexDirection: direction }]}>
+            {items.map((item, index) => (
+                <Pressable
+                    key={item.value}
+                    onPress={() => onPress(item.value)}
+                    style={[
+                        styles.wrapper,
+                        direction === "row" &&
+                            index < lastChildIndex && {
+                                marginRight: gap ? gap : 8,
+                            },
+                        direction !== "row" &&
+                            index < lastChildIndex && {
+                                marginBottom: gap ? gap : 4,
+                            },
+                    ]}
+                >
+                    <View
+                        style={[styles.outer, color && { borderColor: color }]}
+                    >
+                        {initial === item.value && (
+                            <View
+                                style={[
+                                    styles.inner,
+                                    color && { backgroundColor: color },
+                                ]}
+                            ></View>
+                        )}
+                    </View>
+                    <Text style={styles.label}>{item.label}</Text>
+                </Pressable>
+            ))}
+        </View>
+    );
+};
+
+export default RadioButtons;
+
+const styles = StyleSheet.create({
+    // container: {
+    //     flex: 1,
+    //     flexWrap: "wrap",
+    // },
+    wrapper: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    outer: {
+        height: 24,
+        width: 24,
+        borderRadius: 12,
+        borderWidth: 2,
+        justifyContent: "center",
+        alignItems: "center",
+        borderColor: "black",
+        marginRight: 4,
+    },
+    inner: {
+        height: 15,
+        width: 15,
+        borderRadius: 8,
+        backgroundColor: "black",
+    },
+    label: {
+        textTransform: "capitalize",
+    },
+});
